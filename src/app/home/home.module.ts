94373d5566cad99dc9b7ef1894a262c44e0d24f4
Mehandhiga <2k21it36@kiot.ac.in>
2024-09-09 14:24:20 +0530

    creating landing page

---
+++ src/app/home/home.module.ts
@@ -0,0 +1,17 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { HomeRoutingModule } from './home-routing.module';
+import { HeaderComponent } from './header/header.component';
+
+
+@NgModule({
+  declarations: [
+    HeaderComponent
+  ],
+  imports: [
+    CommonModule,
+    HomeRoutingModule
+  ]
+})
+export class HomeModule { }
